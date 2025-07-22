d45e20e74dec61f0f0f233ddaca348849f06adbd
lingzhq <lingzhq@mail2.sysu.edu.cn>
2025-07-22 15:39:35 +0800

    Add an op GroupDiversityFilter

--- data_juicer/ops/filter/__init__.py
+++ data_juicer/ops/filter/__init__.py
@@ -6,6 +6,7 @@ from .average_line_length_filter import AverageLineLengthFilter
 from .character_repetition_filter import CharacterRepetitionFilter
 from .flagged_words_filter import FlaggedWordFilter
 from .general_field_filter import GeneralFieldFilter
+from .group_diversity_filter import GroupDiversityFilter
 from .image_aesthetics_filter import ImageAestheticsFilter
 from .image_aspect_ratio_filter import ImageAspectRatioFilter
 from .image_face_count_filter import ImageFaceCountFilter
@@ -65,6 +66,8 @@ __all__ = [
     "AverageLineLengthFilter",
     "CharacterRepetitionFilter",
     "FlaggedWordFilter",
+    "GeneralFieldFilter",
+    "GroupDiversityFilter",
     "ImageAestheticsFilter",
     "ImageAspectRatioFilter",
     "ImageFaceCountFilter",
@@ -113,7 +116,6 @@ __all__ = [
     "VideoWatermarkFilter",
     "WordRepetitionFilter",
     "WordsNumFilter",
-    "GeneralFieldFilter",
 ]
 
 NON_STATS_FILTERS = [

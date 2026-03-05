2b1018bbb2beaf9c442b8b8a90ac3dda9f9487ee
dubin555 <dubin555@gmail.com>
2026-03-05 11:25:27 +0000

    style: fix pre-commit formatting for dict.update() calls
    Collapse multi-line dict.update() calls to single line to satisfy
    the project's pre-commit formatter.

--- data_juicer/ops/filter/image_face_count_filter.py
+++ data_juicer/ops/filter/image_face_count_filter.py
@@ -68,8 +68,7 @@ class ImageFaceCountFilter(Filter):
         self.max_face_count = max_face_count
 
         self.extra_kwargs = self._default_kwargs.copy()
-        self.extra_kwargs.update(
-            (k, v) for k, v in kwargs.items() if k in self.extra_kwargs)
+        self.extra_kwargs.update((k, v) for k, v in kwargs.items() if k in self.extra_kwargs)
 
         if any_or_all not in ["any", "all"]:
             raise ValueError(f"Keep strategy [{any_or_all}] is not supported. " f'Can only be one of ["any", "all"].')
--- data_juicer/ops/filter/video_motion_score_filter.py
+++ data_juicer/ops/filter/video_motion_score_filter.py
@@ -116,8 +116,7 @@ class VideoMotionScoreFilter(Filter):
         self.relative = relative
 
         self.extra_kwargs = self._default_kwargs.copy()
-        self.extra_kwargs.update(
-            (k, v) for k, v in kwargs.items() if k in self.extra_kwargs)
+        self.extra_kwargs.update((k, v) for k, v in kwargs.items() if k in self.extra_kwargs)
 
         if any_or_all not in ["any", "all"]:
             raise ValueError(f"Keep strategy [{any_or_all}] is not supported. " f'Can only be one of ["any", "all"].')

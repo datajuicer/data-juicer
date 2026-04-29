46c0119319bae51a5c38351a3ed2ad438b78e002
fengrui-z <drake.zfr@gmail.com>
2026-04-29 16:43:02 +0800

    style: apply black formatting

--- data_juicer/core/data/ray_dataset.py
+++ data_juicer/core/data/ray_dataset.py
@@ -441,23 +441,15 @@ class JSONStreamDatasource(_JSONDatasourceBase):
                         schema = batch.schema
                         table = pyarrow.Table.from_batches([batch])
                     elif schema.equals(batch.schema):
-                        table = pyarrow.Table.from_batches(
-                            [batch], schema=schema
-                        )
+                        table = pyarrow.Table.from_batches([batch], schema=schema)
                     else:
                         try:
-                            schema = pyarrow.unify_schemas(
-                                [schema, batch.schema]
-                            )
-                        except (pyarrow.lib.ArrowInvalid,
-                                pyarrow.lib.ArrowTypeError) as e:
+                            schema = pyarrow.unify_schemas([schema, batch.schema])
+                        except (pyarrow.lib.ArrowInvalid, pyarrow.lib.ArrowTypeError) as e:
                             raise ValueError(
-                                f"Schema incompatibility in {path}: {e}. "
-                                f"Cannot unify {schema} with {batch.schema}"
+                                f"Schema incompatibility in {path}: {e}. " f"Cannot unify {schema} with {batch.schema}"
                             ) from e
-                        table = pyarrow.Table.from_batches(
-                            [batch], schema=schema
-                        )
+                        table = pyarrow.Table.from_batches([batch], schema=schema)
                     yield table
                 except StopIteration:
                     return
